e9e5c0aca780256489f20fcf9ab23aa73c7af689
chenyang_qi <1057962686@qq.com>
2019-12-05 16:26:21 +0800

    Finish softmax

--- spring1819_assignment1/assignment1/cs231n/classifiers/linear_svm.py
+++ spring1819_assignment1/assignment1/cs231n/classifiers/linear_svm.py
@@ -32,7 +32,6 @@ def svm_loss_naive(W, X, y, reg):
         correct_class_score = scores[y[i]]
         for j in range(num_classes):
             if j == y[i]:
-
                 continue
             margin = scores[j] - correct_class_score + 1 # note delta = 1
             if margin > 0:
--- spring1819_assignment1/assignment1/cs231n/classifiers/softmax.py
+++ spring1819_assignment1/assignment1/cs231n/classifiers/softmax.py
@@ -32,11 +32,31 @@ def softmax_loss_naive(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
+    scores = X.dot(W) #(N,C)
+    scores = scores - np.amax(scores, axis=1).reshape(-1,1)
+    num_train = X.shape[0]
+    num_classes = W.shape[1]
+    softmax = np.zeros(num_train) 
+    for i in range(num_train):
+      softmax[i] = np.exp(scores[i, y[i]]) / np.sum(np.exp(scores[i]))
+    loss += -np.sum(np.log(softmax))
+    loss /= num_train
+    loss += reg*np.sum(np.square(W))
     pass
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
+    #print("softmax shape", softmax.shape)
+    #print("x[i]", X[i].shape)
+    #print("Denominator", (np.exp(X[i].dot(W[:,y[i]]))**2).shape)
+    for i in range(num_train):
+      for j in range(num_classes):
+        softmax[i] = np.exp(scores[i,j]) / np.sum(np.exp(scores[i]), axis=0)
+        if j == y[i]:
+          dW[:, j] += (-1 + softmax[i]) * X[i]
+        else:
+          dW[:, j] += softmax[i] * X[i]
+    dW /= num_train
+    dW += 2*reg*W
     return loss, dW
 
 
@@ -57,7 +77,19 @@ def softmax_loss_vectorized(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
+    num_train = X.shape[0]
+    num_classes = W.shape[1]
+    scores = X.dot(W)
+    scores_shifted = scores - np.max(scores, axis=1).reshape(-1,1)
+    softmax = np.exp(scores_shifted) / np.sum(np.exp(scores_shifted), axis=1).reshape(-1,1)
+    loss = -1.0* np.sum(np.log(softmax[range(num_train), y]))
+    loss /= num_train
+    loss += reg*np.sum(W**2)
+    
+    softmax[range(num_train), y] -= 1
+    dW = (X.T).dot(softmax)
+    dW /= num_train
+    dW += 2*reg*W
     pass
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
